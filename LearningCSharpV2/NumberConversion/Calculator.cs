d5da3315e55bfb67938e8be9ff3de731dd64a4d2
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-11-19 21:50:01 +0100

    New number conversion projects

---
+++ LearningCSharpV2/NumberConversion/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberConversion
+{
+    public class Calculator
+    {
+        public string ReverseText(string text)
+        {
+            if(text == null)
+            {
+                return null;
+            }
+            else
+            {
+                char[] array = text.ToCharArray();
+                Array.Reverse(array);
+                return new String(array);
+            }
+        }
+
+        public int pow(int num1, int num2)
+        {
+            int result = num1;
+            if (num2 ==0)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 1; i < num2; i++)
+                {
+                    result = result * num1;
+                }
+                return result;
+            }
+            
+        }
+    }
+}
---
+++ LearningCSharpV2/NumberConversion/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace NumberConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number conversion.");
+            Console.WriteLine("-------------------------------------------------");
+
+            // [Marked] Input and Check a number.
+            Console.WriteLine("Please enter a number!");
+            int num;
+            string inputS = Console.ReadLine();
+            while(!int.TryParse(inputS,out num))
+            {
+                Console.WriteLine("Invalid input. Please try again!");
+                Console.WriteLine("Please enter a number!");
+                inputS = Console.ReadLine();
+            }
+            //[/Marked]
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("What do u want to do?");
+            Console.WriteLine("1> Convert Decimal to Binary.");
+            Console.WriteLine("2> Convert Binary to Decimal.");
+
+            string choiceS = Console.ReadLine();
+            switch(choiceS)
+            {
+
+             // [Marked] Decimal to binary.
+                case "1":
+                    if (num<0)
+                    {
+                        Console.WriteLine("Invalid number to convert.");
+                    }
+                    else
+                    {
+                        string result = "";
+                        int remainder;
+                        while (num>0)
+                        {
+                            remainder = num % 2;
+                            num /= 2;
+                            result = result + remainder.ToString();
+                        }
+                        Calculator rev = new Calculator();
+                        result = rev.ReverseText(result);
+                        Console.WriteLine(result);
+                        
+                    }
+                    break;
+                // [/Marked]
+
+                // [Marked] Binary to Decimal.
+                case "2":
+                    if (num < 0)
+                    {
+                        Console.WriteLine("Invalid number to convert.");
+                    }
+                    else
+                    {                        
+                        int resultD = 0;
+                        int count = 0;
+                        int remainder;
+                        Calculator pow = new Calculator();
+                        
+                        while (num > 0)
+                        {
+                            
+                            remainder = num % 2;
+                            if (remainder == 1)
+                            {
+                                resultD += pow.pow(2, count);
+                            }
+                            count++;
+                            num /= 10;                            
+                        }
+                        
+                        Console.WriteLine(resultD);
+                    }
+                    break;
+                // [/Marked]
+            }
+
+
+
+        }
+    }
+}
