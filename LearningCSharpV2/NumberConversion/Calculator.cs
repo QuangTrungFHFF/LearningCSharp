8778452d56bb07c374c2134d4cea2472c93ffade
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-11-20 22:56:15 +0100

    Update (dec to hex) and (hex to dec)

--- LearningCSharpV2/NumberConversion/Calculator.cs
+++ LearningCSharpV2/NumberConversion/Calculator.cs
@@ -37,5 +37,39 @@ namespace NumberConversion
             }
             
         }
+
+        public string hexTrans(int num)
+        {
+            string[] arrayS = new string[16] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+            string result = "error";
+            if (num<0)
+            {
+                return result;
+            }
+            else
+            {
+                result = arrayS[num];
+                return result;
+            }
+        }
+
+        public int hexTransReverse(char c)
+        {
+            string[] arrayS = new string[16] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+            string result = c.ToString();
+            string sS = null;
+            int resultD=0;
+            for (int i =0; i<16;i++)
+            {
+                sS = arrayS[i];
+                if (result == sS)
+                {
+                    resultD = i;
+                    break;
+                }
+            }
+            return resultD;
+            
+        }
     }
 }
--- LearningCSharpV2/NumberConversion/Program.cs
+++ LearningCSharpV2/NumberConversion/Program.cs
@@ -7,26 +7,43 @@ namespace NumberConversion
         static void Main(string[] args)
         {
             Console.WriteLine("Number conversion.");
-            Console.WriteLine("-------------------------------------------------");
-
-            // [Marked] Input and Check a number.
-            Console.WriteLine("Please enter a number!");
-            int num;
-            string inputS = Console.ReadLine();
-            while(!int.TryParse(inputS,out num))
-            {
-                Console.WriteLine("Invalid input. Please try again!");
-                Console.WriteLine("Please enter a number!");
-                inputS = Console.ReadLine();
-            }
-            //[/Marked]
-            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------");            
             Console.WriteLine("What do u want to do?");
             Console.WriteLine("1> Convert Decimal to Binary.");
             Console.WriteLine("2> Convert Binary to Decimal.");
-
+            Console.WriteLine("3> Convert Decimal to Hexadecimal.");
+            Console.WriteLine("4> Convert Hexadecimal to Decimal.");
+            
             string choiceS = Console.ReadLine();
-            switch(choiceS)
+            int num=0;
+            string inputK = null;
+            if (int.Parse(choiceS)<4)
+            {
+                Console.WriteLine("-------------------------------------------------");
+
+                // [Marked] Input and Check a number.
+                Console.WriteLine("Please enter a number!");
+                string inputS = Console.ReadLine();
+
+                while (!int.TryParse(inputS, out num))
+                {
+                    Console.WriteLine("Invalid input. Please try again!");
+                    Console.WriteLine("Please enter a number!");
+                    inputS = Console.ReadLine();
+                }
+                //[/Marked]
+            }
+            else
+            {
+                Console.WriteLine("-------------------------------------------------");
+                Console.WriteLine("Please enter a hex number!");
+                string inputS = Console.ReadLine();
+                inputK = inputS;
+            }
+
+
+
+            switch (choiceS)
             {
 
              // [Marked] Decimal to binary.
@@ -82,6 +99,54 @@ namespace NumberConversion
                     }
                     break;
                 // [/Marked]
+
+                // [Marked] Decimal to Hex.
+                case "3":
+                    if (num < 0)
+                    {
+                        Console.WriteLine("Invalid number to convert.");
+                    }
+                    else
+                    {
+                        string result = null;                        
+                        int remainder;
+                        Calculator hex = new Calculator();
+
+                        while (num > 0)
+                        {
+
+                            remainder = num % 16;                            
+                            num /= 16;
+                            result += hex.hexTrans(remainder);
+                        }
+                        result = hex.ReverseText(result);
+                        Console.WriteLine(result);
+                    }
+                    break;
+                // [/Marked]
+
+                // [Marked] Hexadecimal to Decimal.
+                case "4":
+                    if (inputK == null)
+                    {
+                        Console.WriteLine("Invalid number to convert.");
+                    }
+                    else
+                    {
+                        int count = 0;
+                        int resultD = 0;                        
+                        Calculator pow = new Calculator();
+                        inputK = pow.ReverseText(inputK);
+                        
+                        foreach(char c in inputK)
+                        {
+                            resultD += pow.hexTransReverse(c)*pow.pow(16,count);
+                            count++;
+                        }        
+                        Console.WriteLine(resultD);
+                    }
+                    break;
+                    // [/Marked]
             }
 
 
