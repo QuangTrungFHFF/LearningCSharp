938bd760bd92f2537934c2c86d160da31906af3a
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-06 21:51:59 +0100

    Chap11 ex 6

--- OOPLearningV1/OOPLearningV1/Calculator.cs
+++ OOPLearningV1/OOPLearningV1/Calculator.cs
@@ -11,5 +11,24 @@ namespace OOPLearningV1
             double hypotenuse = Math.Sqrt((Math.Pow(sideA,2)+Math.Pow(sideB,2)));
             return hypotenuse; 
         }
+
+        public double GetAreaFromThreeSide(double sideA, double sideB, double sideC)
+        {
+            double average = (sideA + sideB + sideC) / 2;
+            double result = Math.Sqrt(average*(average- sideA) * (average-sideB)*(average-sideC));
+            return result;
+        }
+
+        public double GetAreaFromSideAndAltitude(double sideA, double altitude)
+        {            
+            double result = (sideA*altitude)/2;
+            return result;
+        }
+
+        public double GetAreaFromSideAndAngle(double sideA, double sideB, double angle)
+        {
+            double result = (sideA * sideB*Math.Sin((angle*Math.PI)/180)) / 2;
+            return result;
+        }
     }
 }
--- OOPLearningV1/OOPLearningV1/Program.cs
+++ OOPLearningV1/OOPLearningV1/Program.cs
@@ -11,8 +11,9 @@ namespace OOPLearningV1
             Console.WriteLine("1. Check leap year.");
             Console.WriteLine("2. Generates and prints on the console 10 random numbers in the range [100, 200].");
             Console.WriteLine("3. Write a program, which prints, on the console which day of the week is today.");
-            Console.WriteLine("4. standard output the count of days, hours, and minutes, which have passes since the computer is started until the moment of the program execution.");
+            Console.WriteLine("4. standard output the count of days, hours, and minutes, which have passes since the computer is started.");
             Console.WriteLine("5. Finds the hypotenuse of a right triangle.");
+            Console.WriteLine("6. Calculates the area of a triangle.");
 
             int chooseInput = int.Parse(Console.ReadLine());
             Calendar calendar = new Calendar();
@@ -79,6 +80,103 @@ namespace OOPLearningV1
                     double hypotenuse = calculator.GetHypotenuse(sideA, sideB);
                     Console.WriteLine("Hypotenuse is: {0:N2}",hypotenuse);
                     Console.ReadKey(true);
+                    break;
+                //6. Calculates the area of a triangle.
+                case 6:
+                    Console.WriteLine("What do u have?");
+                    Console.WriteLine("1> Three sides;");
+                    Console.WriteLine("2> Side and the altitude to it");
+                    Console.WriteLine("3> Two sides and the angle between them in degrees.");
+                    int chooseMethod = int.Parse(Console.ReadLine());
+                    double area;
+                    
+                    switch (chooseMethod)
+                    {
+                        case 1:
+                            //Enter the first side of the triangle
+                            Console.WriteLine("Please enter the first side: ");
+                            string sSideAx = Console.ReadLine();
+                            double sideAx;
+                            while (!double.TryParse(sSideAx, out sideAx))
+                            {
+                                Console.WriteLine("Invalid input. Please enter again!");
+                                sSideAx = Console.ReadLine();
+                            }
+                            //Enter the second side of the triangle
+                            Console.WriteLine("Please enter the second side: ");
+                            string sSideBx = Console.ReadLine();
+                            double sideBx;
+                            while (!double.TryParse(sSideBx, out sideBx))
+                            {
+                                Console.WriteLine("Invalid input. Please enter again!");
+                                sSideBx = Console.ReadLine();
+                            }
+                            //Enter the third side of the triangle
+                            Console.WriteLine("Please enter the third side: ");
+                            string sSideCx = Console.ReadLine();
+                            double sideCx;
+                            while (!double.TryParse(sSideCx, out sideCx))
+                            {
+                                Console.WriteLine("Invalid input. Please enter again!");
+                                sSideCx = Console.ReadLine();
+                            }
+                            area = calculator.GetAreaFromThreeSide(sideAx, sideBx, sideCx);
+                            Console.WriteLine("Area of the triangle is: {0:N2}",area);
+                            break;
+                        case 2:
+                            //Enter the first side of the triangle
+                            Console.WriteLine("Please enter the first side: ");
+                            string sSideF = Console.ReadLine();
+                            double sideF;
+                            while (!double.TryParse(sSideF, out sideF))
+                            {
+                                Console.WriteLine("Invalid input. Please enter again!");
+                                sSideF = Console.ReadLine();
+                            }
+                            //Enter the altitude of that side
+                            Console.WriteLine("Please enter the altitude: ");
+                            string sAltitude = Console.ReadLine();
+                            double altitude;
+                            while (!double.TryParse(sAltitude, out altitude))
+                            {
+                                Console.WriteLine("Invalid input. Please enter again!");
+                                sAltitude = Console.ReadLine();
+                            }
+                            area = calculator.GetAreaFromSideAndAltitude(sideF,altitude);
+                            Console.WriteLine("Area of the triangle is: {0:N2}", area);
+                            break;
+                        case 3:
+                            //Enter the first side of the triangle
+                            Console.WriteLine("Please enter the first side: ");
+                            string sSideFirst = Console.ReadLine();
+                            double sideFirst;
+                            while (!double.TryParse(sSideFirst, out sideFirst))
+                            {
+                                Console.WriteLine("Invalid input. Please enter again!");
+                                sSideFirst = Console.ReadLine();
+                            }
+                            //Enter the second side of the triangle
+                            Console.WriteLine("Please enter the second side: ");
+                            string sSideSecond = Console.ReadLine();
+                            double sideSecond;
+                            while (!double.TryParse(sSideSecond, out sideSecond))
+                            {
+                                Console.WriteLine("Invalid input. Please enter again!");
+                                sSideSecond = Console.ReadLine();
+                            }
+                            //Enter the angle between those 2 sides
+                            Console.WriteLine("Please Enter the angle between those 2 sides ");
+                            string sAngle = Console.ReadLine();
+                            double angle;
+                            while (!double.TryParse(sAngle, out angle))
+                            {
+                                Console.WriteLine("Invalid input. Please enter again!");
+                                sAngle = Console.ReadLine();
+                            }
+                            area = calculator.GetAreaFromSideAndAngle(sideFirst, sideSecond, angle);
+                            Console.WriteLine("Area of the triangle is: {0:N2}", area);
+                            break;
+                    }
                     break;
             }
         }
