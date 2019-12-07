ab29ec8733898d31c8b1acffc5e892500f2103bd
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-07 12:47:46 +0100

    Chap11 Ex7

---
+++ OOPLearningV1/CountCat/Program.cs
@@ -0,0 +1,24 @@
+﻿using CreatingAndUsingObjects;
+using System;
+
+namespace CountCat
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Let's count the cat.");
+            Cat someCat = new Cat();
+            someCat.SayMiau();
+            Sequence.NextValue();
+
+            Console.WriteLine("The color of cat {0} is {1}.",someCat.Name, someCat.Color);
+            Cat someCat2 = new Cat("Johnny", "brown");
+            someCat2.SayMiau();
+            Console.WriteLine("The color of cat {0} is {1}.",someCat2.Name, someCat2.Color);
+            int catNumber = Sequence.NextValue();
+            Console.WriteLine("There are {0} cat(s).", catNumber);
+        }
+    }
+}
