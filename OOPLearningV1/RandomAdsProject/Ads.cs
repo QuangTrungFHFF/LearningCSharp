7106ae03e7d5cd88088ca24e356c21ed4eed8d9f
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-08 12:46:31 +0100

    Chap 11 ex 11. Generate random Ads

---
+++ OOPLearningV1/RandomAdsProject/Ads.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomAdsProject
+{
+    internal class Ads
+    {
+        private List<string> laudatoryPhrases = new List<string> { "The product is excellent.", "This is a great product.", "I use this product constantly.", "This is the best product from this category." };
+        private List<string> laudatoryStories = new List<string> { "Now I feel better.", "I managed to change.", "It made some miracle.", "I can’t believe it, but now I am feeling great.", "You should try it, too. I am very satisfied." };
+        private List<string> firstName = new List<string> { "Dayan", "Stella", "Hellen", "Kate" };
+        private List<string> lastName = new List<string> { "Johnson", "Peterson", "Charls" };
+        private List<string> cities = new List<string> { "London", "Paris", "Berlin", "New York", "Madrid" };
+
+        public void GenerateRandomAds()
+        {
+            Random ran = new Random();
+            string lauPh = this.laudatoryPhrases[ran.Next(0, laudatoryPhrases.Count)];
+            string lauSt = this.laudatoryStories[ran.Next(0, laudatoryStories.Count)] + " -- ";
+            string firstN = this.firstName[ran.Next(0, firstName.Count)];
+            string lastN = this.lastName[ran.Next(0, lastName.Count)]+ ", ";
+            string city = this.cities[ran.Next(0, cities.Count)];
+            string[] ads = new string[] {lauPh,lauSt,firstN,lastN,city };
+            string randomAds = string.Join(" ",ads);
+            Console.WriteLine(randomAds);
+        }
+    }
+}
---
+++ OOPLearningV1/RandomAdsProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RandomAdsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many ads do u want to generate?");
+            int count = int.Parse(Console.ReadLine());
+            for(int i =0; i<count;i++)
+            {
+                Ads randomAds = new Ads();
+                randomAds.GenerateRandomAds();
+            }
+        }
+    }
+}
