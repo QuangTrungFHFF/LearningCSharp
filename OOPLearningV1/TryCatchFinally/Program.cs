fdb526f1623389a37dc4a6434da02a8f7800d57a
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-10 17:35:00 +0100

    Chap 12 exercise 7

---
+++ OOPLearningV1/TryCatchFinally/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+            double result;
+            Console.WriteLine("Please enter a number!");
+           try
+            {
+                number = int.Parse(Console.ReadLine());
+                if(number>=0)
+                {
+                    result = Math.Sqrt(number);
+                    Console.WriteLine("Result is: {0}", result);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is  <0 ",number);
+                }
+                
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Wrong format!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
