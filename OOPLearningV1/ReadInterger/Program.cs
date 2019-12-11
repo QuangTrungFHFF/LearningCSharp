8cc1942ff3e47340b4aa8471675943a6bc13bb06
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2019-12-11 19:12:18 +0100

    Chap12 Ex 8

---
+++ OOPLearningV1/ReadInterger/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReadInterger
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int min = 0;
+            int num;
+            var numberList = new List<int>();
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Please enter a number!");
+                    num = ReadNumber(min, 100);
+                    numberList.Add(num);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Input is not an integer number!" + ex.Message);
+                throw;
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                throw;
+            }
+            foreach (int j in numberList)
+            {
+                Console.WriteLine("");
+                Console.Write(" " + j);
+            }
+        }
+
+        private static int ReadNumber(int start, int end)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n < start || n > end)
+            {
+                throw new ArgumentOutOfRangeException("Number out of range!");
+            }
+            else
+            {
+                return n;
+            }
+        }
+    }
+}
